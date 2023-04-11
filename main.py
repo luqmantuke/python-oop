d42455de8d95dc311f8b08db6edf9d6095629334
Luqman Tuke <lsuleiman2002@gmail.com>
2023-04-11 20:39:55 +0300

    encapsulation

---
+++ main.py
@@ -0,0 +1,35 @@
+class Book:
+    def __init__(self, title,quantity,author,price):
+        self.title = title
+        self.quantity = quantity
+        self.author = author
+        self.__price = price
+        self.__discount = None
+
+
+    def set_discount(self,discount):
+        self.__discount = discount
+
+    
+    def get_price(self):
+        if self.__discount:
+            return self.__price * self.quantity * (1-self.__discount)
+        
+        return self.__price * self.quantity
+        
+
+    def __repr__(self):
+       return f"Book: {self.title}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.get_price()}"
+
+
+single_book = Book('Two States', 1, 'Chetan Bhagat', 200)
+
+bulk_books = Book('Two States', 25, 'Chetan Bhagat', 200)
+
+bulk_books.set_discount(0.20)
+
+
+print(single_book.get_price())
+print(bulk_books.get_price())
+print(single_book)
+print(bulk_books)
